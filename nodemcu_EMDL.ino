7a0608703e0c37e560c5f97dbe24cc50be4072d0
Erick Escobar <erick_093@hotmail.com>
2017-05-04 23:28:53 -0400

    Added support for Energy Calculations

--- nodemcu_EMDL.ino
+++ nodemcu_EMDL.ino
@@ -20,14 +20,16 @@ LOCAL os_timer_t myTimer;
 unsigned long V1chan = 241543;
 unsigned long A1chan= 254879;
 
-int maxcounts = 60 ;
-int cont = 0 ;
 /* Channel numbers of TS*/
 const char * myWriteAPIKey = "XZATGLWSQ6FIJA8K"; //API Key of TS
-char result[8];
+char result_V[8];
+char result_A[8];
+char result_P[8];
+char result_E[8];
 bool tickOccured;
 float voltage;
-
+int maxcounts = 60 ;
+int cont = 0 ;
 
 
 void timerCallback(void *pArg) {
@@ -38,7 +40,7 @@ void timerCallback(void *pArg) {
 void user_init(void) {
   //os_timer_disarm(&myTimer);
   os_timer_setfn(&myTimer, timerCallback, NULL);
-  os_timer_arm(&myTimer, 15000, true);
+  os_timer_arm(&myTimer, 60000, true); //60 seconds interrupt
 }
 
 void setup() {
@@ -56,9 +58,9 @@ void loop() {
   //Serial.println(WiFi.status());
   //Serial.println(client.state());
 
-  int sensorValue = analogRead(A0);
-  voltage = sensorValue * (VOLTAGE_MAX / VOLTAGE_MAXCOUNTS) ;
-  _Voltage(1);
+  //int sensorValue = analogRead(A0);
+  //voltage = sensorValue * (VOLTAGE_MAX / VOLTAGE_MAXCOUNTS) ;
+  
   //Recconecting Phase
   if(WiFi.status() == 6 ){
     Serial.println("Reconnecting WiFi");
@@ -69,25 +71,81 @@ void loop() {
     connectMQTT();
   }
 
+
   //Send values to OPENHAB2(MQTT) and TS servers
-  Serial.print("Sending (to MQTT) value: ");
-  Serial.println(voltage,5);
-  dtostrf(voltage , 6, 5, result); //converting FLOAT to CHAR
-  if (client.publish(TOPIC_V, result)) {
-    Serial.println("Publish ok");
-  }
-  else {
-    Serial.println("Publish failed");
-  }
+  //Serial.print("Sending (to MQTT) value: ");
+  //Serial.println(voltage,5);
+  //dtostrf(voltage , 6, 5, result); //converting FLOAT to CHAR
+  // if (client.publish(TOPIC_V, result)) {
+  //   Serial.println("Publish ok");
+  // }
+  // else {
+  //   Serial.println("Publish failed");
+  // }
+
 
   if (tickOccured == true)
   {
     Serial.println("Tick Occurred");
-    Serial.print("Sending(to TS) value: ");
-    Serial.println(voltage,5);
-    ThingSpeak.writeField(V1chan, 1, voltage, myWriteAPIKey);
+    // Serial.print("Sending(to TS) value: ");
+    // Serial.println(voltage,5);
+    // ThingSpeak.writeField(V1chan, 1, voltage, myWriteAPIKey);
+
+    //Reading Voltage from pin 1 (Multiplexer)
+  	double read_V=_Voltage(1);
+  	Serial.print("Sending Voltage: ");
+  	Serial.println(read_V,5);
+  	dtostrf(read_V, 6, 5, result_V); //converting FLOAT to CHAR
+  	//Reading Current from pin 2 (Multiplexer)
+  	double read_A=_Current(2);
+  	Serial.print("Sending Amperage: ");
+  	Serial.println(read_A,5);
+  	dtostrf(read_A, 6, 5, result_A); //converting FLOAT to CHAR
+  	//Obtaining Power
+  	double read_P=_Power(read_V,read_A);
+  	Serial.print("Sending Power: ");
+  	Serial.println(read_P,5);
+  	dtostrf(read_P, 6, 5, result_P); //converting FLOAT to CHAR
+
+  	double energy = energy + (read_P/maxcounts);
+
+    if (client.publish(TOPIC_V, result_V)) {
+    	Serial.println("Publish V ok");
+  	}
+  	else {
+    	Serial.println("Publish V failed");
+  	}
+    if (client.publish(TOPIC_A, result_A)) {
+    	Serial.println("Publish A ok");
+  	}
+  	else {
+    	Serial.println("Publish A failed");
+  	}
+    if (client.publish(TOPIC_P, result_P)) {
+    	Serial.println("Publish P ok");
+    	cont++;
+  	}
+  	else {
+    	Serial.println("Publish P failed");
+  	}
+
+  	if (cont == maxcounts){
+	  	dtostrf(energy, 6, 5, result_E); //converting FLOAT to CHAR
+  		Serial.println("Maxcounts reached");
+	    if (client.publish(TOPIC_E, result_E)) {
+    		Serial.println("Publish E ok");
+    		cont = 0 ;
+    		energy = 0;
+		}
+  		else {
+    		Serial.println("Publish E failed");
+  		}
+
+  	}
     tickOccured = false;
   }
+
+
   client.loop();
   delay(500);
   yield();
